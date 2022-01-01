996f5eb4a39afbd44453a23e2e35af6536247617
Syed Haider <syedshahbaz@live.co.uk>
2022-01-01 01:00:31 +0100

    Added new components and fixed nav bar

--- SocialNetworkingApp/ClientApp/src/app/app.module.ts
+++ SocialNetworkingApp/ClientApp/src/app/app.module.ts
@@ -12,17 +12,23 @@ import { HomeComponent } from "./home/home.component";
 import { CounterComponent } from "./counter/counter.component";
 import { FetchDataComponent } from "./fetch-data/fetch-data.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from "./register/register.component";
+import { ListsComponent } from "./lists/lists.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { MemberListComponent } from "./members/member-list/member-list.component";
+import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-      RegisterComponent
-   ],
+    RegisterComponent,
+    ListsComponent,
+    MessagesComponent,
+  ],
   imports: [
     BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
@@ -33,6 +39,11 @@ import { RegisterComponent } from './register/register.component';
       { path: "", component: HomeComponent, pathMatch: "full" },
       { path: "counter", component: CounterComponent },
       { path: "fetch-data", component: FetchDataComponent },
+      { path: "members", component: MemberListComponent },
+      { path: "members/:id", component: MemberDetailComponent },
+      { path: "lists", component: ListsComponent },
+      { path: "messages", component: MessagesComponent },
+      { path: "**", component: HomeComponent, pathMatch: "full" },
     ]),
     BrowserAnimationsModule,
   ],
